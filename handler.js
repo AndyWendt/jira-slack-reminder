3814c98163f0f23dbc146ebddfd0422783bab419
Andy Wendt <andy@awendt.com>
2018-09-24 20:06:19 -0600

    Update serverless to include environment variables

--- handler.js
+++ handler.js
@@ -2,5 +2,6 @@
 
 module.exports.run = (event, context) => {
   const time = new Date();
+  console.log(process.env.jiraSearchUrl);
   console.log(`Your cron function "${context.functionName}" ran at ${time}`);
 };
